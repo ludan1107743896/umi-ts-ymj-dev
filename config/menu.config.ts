183f62d44db7bdb27cfeddd5cb104fba7aa6e047
ludan <1107743896@qq.com>
2020-09-14 17:46:29 +0800

    init

---
+++ config/menu.config.ts
@@ -0,0 +1,24 @@
+export default [
+  {
+    title: 'Dashboard',
+    link: '/dashboard',
+    key: 'dashboard',
+    icon: '',
+    children: [],
+  },
+  {
+    title: '列表页',
+    link: '/list',
+    key: 'list',
+    icon: '',
+    children: [
+      {
+        title: '查询列表',
+        link: '/list/queryTable',
+        key: 'queryTable',
+        icon: '',
+        children: [],
+      },
+    ],
+  },
+];
